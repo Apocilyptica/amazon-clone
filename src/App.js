f042ba628fa931f59962cc1856ddccdad8721e20
James Jager <jviper882000@gmail.com>
2021-03-25 16:49:19 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Components
+import Header from "./Components/Header";
+import Cart from "./Components/Cart";
+
+// Pages
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <Header />
+        <Switch>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: #eaeded;
+`;
---
+++ src/Pages/ShoppingCart/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Components
+import Cart from "../../Components/Cart";
+
+const ShoppingCart = () => {
+  return <Cart />;
+};
+
+export default ShoppingCart;
